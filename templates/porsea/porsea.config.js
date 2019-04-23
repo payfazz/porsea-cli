dfe3ce1102aec39b006035582e7a814e3fcd3244
azharprabudi <azharprabui@gmail.com>
2019-04-23 17:47:10 +0700

    change template porsea config

--- templates/porsea/porsea.config.js
+++ templates/porsea/porsea.config.js
@@ -1,19 +1,4 @@
-const path = require("path");
-
-const porseaConfig = webpackConf => {
-  return {
-    customWebpack: {
-      ...webpackConf,
-      resolve: {
-        alias: {
-          Pages: path.resolve(__dirname, "src/pages"),
-          Components: path.resolve(__dirname, "src/components"),
-          Assets: path.resolve(__dirname, "src/assets")
-        }
-      }
-      //Custom Webpack Config Here
-    }
-  };
+module.exports = {
+  env: {},
+  resolvers: {}
 };
-
-module.exports = porseaConfig;
