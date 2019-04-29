2bb2cfd2d367d1e12bb18f66afe8416cdc2f2df0
Willy-Laptop <w.praditya@yahoo.com>
2019-04-29 20:00:12 +0700

    added default resolvers

--- templates/porsea/package.json.js
+++ templates/porsea/package.json.js
@@ -2,6 +2,18 @@ module.exports = ({ name, description }) => ({
   name,
   version: "1.0.0",
   description,
+  jest: {
+    moduleNameMapper: {
+      "^.+\\.scss$": "babel-jest",
+      "^Apis(.*)$": "<rootDir>/src/apis$1",
+      "^Assets(.*)$": "<rootDir>/src/assets$1",
+      "^Components(.*)$": "<rootDir>/src/components$1",
+      "^Pages(.*)$": "<rootDir>/src/pages$1",
+      "^Services(.*)$": "<rootDir>/src/services$1",
+      "^Styles(.*)$": "<rootDir>/src/styles$1",
+      "^Utils(.*)$": "<rootDir>/src/utils$1"
+    }
+  },
   scripts: {
     start: "porsea start",
     build: "porsea build"
--- templates/porsea/porsea.config.js
+++ templates/porsea/porsea.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   env: {},
-  resolvers: {}
+  resolvers: {
+    Apis: "./src/apis",
+    Assets: "./src/assets",
+    Components: "./src/components",
+    Pages: "./src/pages",
+    Services: "./src/services",
+    Styles: "./src/styles",
+    Utils: "./src/utils"
+  }
 };
