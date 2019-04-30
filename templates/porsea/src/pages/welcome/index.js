1e8084a9a68bc3dc409816dd0003686e836fb9af
azharprabudi <azharprabui@gmail.com>
2019-04-30 16:37:59 +0700

    new example for welcome page

--- templates/porsea/src/pages/welcome/index.js
+++ templates/porsea/src/pages/welcome/index.js
@@ -2,13 +2,6 @@ import React, { Component } from "react";
 import MyComponent from "./components/my-component";
 
 class WelcomePage extends React.Component {
-  static navigationOptions() {
-    return {
-      path: "/",
-      exact: true
-    };
-  }
-
   render() {
     return (
       <div>
